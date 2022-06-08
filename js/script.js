d01bcde8fb2a9e1e45addb2aec1e127a098ba280
Tobias Roland <samu1493@stud.kea.dk>
2022-06-08 13:18:29 +0200

    animation på dosn arrow

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const burger = document.querySelector("button");
 const menu = document.querySelector(".nav-menu");
+const arrow_down_forside = document.querySelector(".scroll_btn_down_White");
+
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  arrow_down_forside.classList.add("bounce");
+}
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
